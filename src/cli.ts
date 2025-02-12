71b81cd2b52ea4e865567bcaef52eb2e6c08982a
Saurabh Batra <sbatra@ciena.com>
2025-02-12 18:13:33 +0530

    Revert cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -6,6 +6,4 @@ import {run} from 'jscodeshift/src/Runner'
 const transformerPath = resolve(__dirname, join('transforms', process.argv[2]))
 const filePaths = process.argv.slice(3)
 
-console.log('transformerPath - ', transformerPath)
-
 run(transformerPath, filePaths, {})
