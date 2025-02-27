5109ecefe5ebd9e66d01af960db86888fbad96eb
Rahul Shanwal <rshanwal@ciena.com>
2025-02-27 10:03:37 +0530

    chore: fix lint errors

--- src/transforms/ember-getWithDefault-to-lodash-get/index.ts
+++ src/transforms/ember-getWithDefault-to-lodash-get/index.ts
@@ -1,78 +1,113 @@
-import {API, FileInfo, ImportSpecifier, JSCodeshift, Options} from 'jscodeshift'
+import {
+  API,
+  FileInfo,
+  ImportSpecifier,
+  JSCodeshift,
+  Options,
+} from "jscodeshift";
 
-export const parser = 'ts'
+export const parser = "ts";
 
-export default function transformer(file: FileInfo, api: API, options: Options) {
-    const j: JSCodeshift = api.jscodeshift
-    const root = j(file.source, options)
-    const getMethodName = 'get'
-    const getwithDefault = 'getWithDefault'
+export default function transformer(
+  file: FileInfo,
+  api: API,
+  options: Options,
+) {
+  const j: JSCodeshift = api.jscodeshift;
+  const root = j(file.source, options);
+  const getMethodName = "get";
+  const getwithDefault = "getWithDefault";
 
-    // check for the usage of getWithDefault() or this.getWithDefault
-    const hasGetWithDefaultUsage = root.find(j.CallExpression, {
-        callee: (callee) => 
-            (callee.type === 'MemberExpression' && callee.property.type === 'Identifier' && callee.property.name === getwithDefault) ||
-            (callee.type === 'Identifier' && callee.name === getwithDefault)
-    }).size() > 0
+  // check for the usage of getWithDefault() or this.getWithDefault
+  const hasGetWithDefaultUsage =
+    root
+      .find(j.CallExpression, {
+        callee: (callee) =>
+          (callee.type === "MemberExpression" &&
+            callee.property.type === "Identifier" &&
+            callee.property.name === getwithDefault) ||
+          (callee.type === "Identifier" && callee.name === getwithDefault),
+      })
+      .size() > 0;
 
-    if (hasGetWithDefaultUsage) {
-        let existingLodashGetImport: string | null = null
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        let emberObjectImportPath: any = null
+  if (hasGetWithDefaultUsage) {
+    let existingLodashGetImport: string | null = null;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let emberObjectImportPath: any = null;
 
-        //Analyse import statements - if lodash - get is already imported then use that
-        root.find(j.ImportDeclaration).forEach((path) => {
-            const importSource = path.value.source.value
-            if (typeof importSource === 'string') {
-                if (['lodash/get', 'lodash-es/get'].includes(importSource)) {
-                    const defaultSpecifier = path.value.specifiers?.find((spec) => spec.type === 'ImportDefaultSpecifier')
-                    if (defaultSpecifier && defaultSpecifier.local) {
-                        existingLodashGetImport = defaultSpecifier.local.name
-                    }
-                } else if (['lodash', 'lodash-es'].includes(importSource)) {
-                    const specifiers = path.value.specifiers?.find((spec) => spec.type === 'ImportSpecifier' && spec.imported.name === getMethodName)
-                    if (specifiers && specifiers.local) {
-                        existingLodashGetImport = specifiers.local.name
-                    }
-                }
-                if (importSource === '@ember/object') {
-                    emberObjectImportPath = path
-                }
-            }
-        })
-
-        // Add lodash/get import
-        if (!existingLodashGetImport) {
-            root.find(j.ImportDeclaration).at(0).insertBefore(
-                j.importDeclaration([
-                    j.importDefaultSpecifier(j.identifier(getwithDefault))
-                ], j.literal('lodash/get'))
-            )
+    //Analyse import statements - if lodash - get is already imported then use that
+    root.find(j.ImportDeclaration).forEach((path) => {
+      const importSource = path.value.source.value;
+      if (typeof importSource === "string") {
+        if (["lodash/get", "lodash-es/get"].includes(importSource)) {
+          const defaultSpecifier = path.value.specifiers?.find(
+            (spec) => spec.type === "ImportDefaultSpecifier",
+          );
+          if (defaultSpecifier && defaultSpecifier.local) {
+            existingLodashGetImport = defaultSpecifier.local.name;
+          }
+        } else if (["lodash", "lodash-es"].includes(importSource)) {
+          const specifiers = path.value.specifiers?.find(
+            (spec) =>
+              spec.type === "ImportSpecifier" &&
+              spec.imported.name === getMethodName,
+          );
+          if (specifiers && specifiers.local) {
+            existingLodashGetImport = specifiers.local.name;
+          }
         }
-
-        //Replace all getWithDefault usages
-        root.find(j.CallExpression, {
-            callee: (callee) => 
-                (callee.type === 'MemberExpression' && callee.property.type === 'Identifier' && callee.property.name === getwithDefault) ||
-                (callee.type === 'Identifier' && callee.name === getwithDefault)
-        }).replaceWith((path) => {
-            const args = path.value.arguments
-            const replacementIdentifier = existingLodashGetImport || getwithDefault
-            const newArgs = path.value.callee.type === 'MemberExpression' ? [path.value.callee.object, ...args] : args
-            return j.callExpression(j.identifier(replacementIdentifier), newArgs)
-        })
-
-        // Remove getWithDefault from @ember/object imports
-        if (emberObjectImportPath) {
-            emberObjectImportPath.value.specifiers =emberObjectImportPath.value.specifiers?.filter(
-                (spec: ImportSpecifier) => spec.type !== 'ImportSpecifier' || spec.imported.name !== getwithDefault
-            )
-    
-            if(emberObjectImportPath.value.specifiers.length === 0) {
-                j(emberObjectImportPath).remove()
-            }
+        if (importSource === "@ember/object") {
+          emberObjectImportPath = path;
         }
+      }
+    });
+
+    // Add lodash/get import
+    if (!existingLodashGetImport) {
+      root
+        .find(j.ImportDeclaration)
+        .at(0)
+        .insertBefore(
+          j.importDeclaration(
+            [j.importDefaultSpecifier(j.identifier(getwithDefault))],
+            j.literal("lodash/get"),
+          ),
+        );
     }
 
-    return root.toSource({quote: 'single', objectCurlySpacing: false})
+    //Replace all getWithDefault usages
+    root
+      .find(j.CallExpression, {
+        callee: (callee) =>
+          (callee.type === "MemberExpression" &&
+            callee.property.type === "Identifier" &&
+            callee.property.name === getwithDefault) ||
+          (callee.type === "Identifier" && callee.name === getwithDefault),
+      })
+      .replaceWith((path) => {
+        const args = path.value.arguments;
+        const replacementIdentifier = existingLodashGetImport || getwithDefault;
+        const newArgs =
+          path.value.callee.type === "MemberExpression"
+            ? [path.value.callee.object, ...args]
+            : args;
+        return j.callExpression(j.identifier(replacementIdentifier), newArgs);
+      });
+
+    // Remove getWithDefault from @ember/object imports
+    if (emberObjectImportPath) {
+      emberObjectImportPath.value.specifiers =
+        emberObjectImportPath.value.specifiers?.filter(
+          (spec: ImportSpecifier) =>
+            spec.type !== "ImportSpecifier" ||
+            spec.imported.name !== getwithDefault,
+        );
+
+      if (emberObjectImportPath.value.specifiers.length === 0) {
+        j(emberObjectImportPath).remove();
+      }
+    }
+  }
+
+  return root.toSource({ quote: "single", objectCurlySpacing: false });
 }
--- src/transforms/ember-getWithDefault-to-lodash-get/tests/transform.test.ts
+++ src/transforms/ember-getWithDefault-to-lodash-get/tests/transform.test.ts
@@ -1,37 +1,45 @@
-import runTestCases from '#utils/testHelper.js'
-import transform, {parser} from '#transforms/ember-getWithDefault-to-lodash-get/index.js'
+import runTestCases from "#utils/testHelper.js";
+import transform, {
+  parser,
+} from "#transforms/ember-getWithDefault-to-lodash-get/index.js";
 
 const testCases = [
   {
-    name: 'should handle ember-getWithDefault-to-lodash-get usage with only getWithDefault import',
-    input: 'transform.input.js',
-    output: 'transform.output.js'
+    name: "should handle ember-getWithDefault-to-lodash-get usage with only getWithDefault import",
+    input: "transform.input.js",
+    output: "transform.output.js",
   },
   {
-    name: 'should handle ember-getWithDefault-to-lodash-get usage with lodash import',
-    input: 'lodash.import.input.js',
-    output: 'lodash.import.output.js'
+    name: "should handle ember-getWithDefault-to-lodash-get usage with lodash import",
+    input: "lodash.import.input.js",
+    output: "lodash.import.output.js",
   },
   {
-    name: 'should handle ember-getWithDefault-to-lodash-get usage with ember get import',
-    input: 'ember.get.input.js',
-    output: 'ember.get.output.js'
+    name: "should handle ember-getWithDefault-to-lodash-get usage with ember get import",
+    input: "ember.get.input.js",
+    output: "ember.get.output.js",
   },
   {
-    name: 'should handle ember-getWithDefault-to-lodash-get usage with lodash get import as alias',
-    input: 'lodash.get.input.js',
-    output: 'lodash.get.output.js'
+    name: "should handle ember-getWithDefault-to-lodash-get usage with lodash get import as alias",
+    input: "lodash.get.input.js",
+    output: "lodash.get.output.js",
   },
   {
-    name: 'should handle ember-getWithDefault-to-lodash-get usage with lodash get import as alias',
-    input: 'lodash.get.input.js',
-    output: 'lodash.get.output.js'
+    name: "should handle ember-getWithDefault-to-lodash-get usage with lodash get import as alias",
+    input: "lodash.get.input.js",
+    output: "lodash.get.output.js",
   },
   {
-    name: 'should handle ember-getWithDefault-to-lodash-get usage with ts',
-    input: 'transform.input.js',
-    output: 'transform.output.js'
-  }
-]
+    name: "should handle ember-getWithDefault-to-lodash-get usage with ts",
+    input: "transform.input.js",
+    output: "transform.output.js",
+  },
+];
 
-runTestCases('ember-getWithDefault-to-lodash-get', __dirname, testCases, transform, parser)
+runTestCases(
+  "ember-getWithDefault-to-lodash-get",
+  __dirname,
+  testCases,
+  transform,
+  parser,
+);
